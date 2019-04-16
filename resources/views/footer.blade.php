663b7851124174caee55d5e2702ff0f1ae4fded2
proyecto <marielba.perezl@gmail.com>
2019-04-16 09:44:13 -0400

    footer pc cantv

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,26 @@
+<!-- Footer -->
+<footer id="footer" class="pb-4 pt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-lg">
+        <a href="#">Preguntas Frecuentes</a>
+      </div>
+      <div class="col-12 col-lg">
+        <a href="#">Contáctanos</a>
+      </div>
+      <div class="col-12 col-lg">
+        <a href="#">Prensa</a>
+      </div>
+      <div class="col-12 col-lg">
+        <a href="#">Privacidad</a>
+      </div>
+      <div class="col-12 col-lg">
+        <a href="#">Estudiantes</a>
+      </div>
+      <div class="col-12 col-lg">
+        <a href="#">Terminos y condiciones</a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- fin footer -->
